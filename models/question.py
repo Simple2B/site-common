8e3bd93ad9c9b1ed42a0bf806bcfe20015be7066
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-07-26 14:40:56 +0300

    save on question

--- models/question.py
+++ models/question.py
@@ -3,17 +3,19 @@ from sqlalchemy import orm
 
 from app.database import db
 
-from .utils import generate_uuid
+from .utils import generate_uuid, ModelMixin
 
 
-class Question(db.Model):
+class Question(db.Model, ModelMixin):
     __tablename__ = "questions"
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
     text: orm.Mapped[str] = orm.mapped_column(sa.String(512), nullable=False)
     correct_answer_mark: orm.Mapped[int] = orm.mapped_column(sa.Integer, nullable=False)
 
-    uid: orm.Mapped[str] = orm.mapped_column(sa.String(128), nullable=False, default=generate_uuid)
+    uid: orm.Mapped[str] = orm.mapped_column(
+        sa.String(128), nullable=False, default=generate_uuid
+    )
 
     variants = orm.relationship("VariantAnswer", viewonly=True)
 
