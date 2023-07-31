4b890a965dba5c44b6d695b7b1eeb1b9d9114dd9
nazarKobryn <kobrinnazar05@gmail.com>
2023-07-31 10:02:27 +0300

    updated model case

--- models/case.py
+++ models/case.py
@@ -1,7 +1,8 @@
 # flake8: noqa F821
 from typing import List
 import sqlalchemy as sa
-from sqlalchemy import orm
+from sqlalchemy import  orm
+from sqlalchemy.ext.hybrid import hybrid_property
 
 
 # must have this import
@@ -13,7 +14,7 @@ class Case(db.Model):
     __tablename__ = "cases"
 
     id: orm.Mapped[int] = orm.mapped_column(sa.Integer, primary_key=True)
-    title: orm.Mapped[str] = orm.mapped_column(sa.String(32), nullable=False)
+    title: orm.Mapped[str] = orm.mapped_column(sa.String(32), nullable=False, unique=True)
     title_image_url: orm.Mapped[str] = orm.mapped_column(sa.String(256), nullable=False)
     sub_title_image_url: orm.Mapped[str] = orm.mapped_column(sa.String(256), nullable=False)
 
@@ -30,6 +31,19 @@ class Case(db.Model):
         secondary=case_stacks, back_populates="_cases", lazy="dynamic", viewonly=True,
     )
     sub_images: orm.Mapped[List["CaseImage"]] = orm.relationship("CaseImage", viewonly=True )
+
+    @hybrid_property
+    def stacks(self) -> str:
+        return  [stack.name for stack in self._stacks.all()]
+
+    @hybrid_property
+    def images(self) -> str:
+        return  [img.url for img in self.sub_images]
+
+    @hybrid_property
+    def slug_name(self) -> str:
+        return  self.title.strip().replace(" ", "-").lower()
+
 
     def __repr__(self):
         return f"<{self.id}: {self.title}>"
