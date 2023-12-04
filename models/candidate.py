993ccca1008036f112daabcc6630547e5bed3216
nazarKobryn <kobrinnazar05@gmail.com>
2023-12-04 12:46:33 +0200

    updated models

--- models/candidate.py
+++ models/candidate.py
@@ -20,7 +20,7 @@ class Candidate(db.Model, BaseUser):
     image_url: orm.Mapped[str] = orm.mapped_column(sa.String(128), nullable=True)
     git_hub_id: orm.Mapped[str] = orm.mapped_column(sa.String(32), nullable=False)
 
-    current_question_id: orm.Mapped[int] = orm.mapped_column(
+    current_question_id: orm.Mapped[int | None] = orm.mapped_column(
         sa.Integer, nullable=True, default=None
     )
     quiz_score: orm.Mapped[float] = orm.mapped_column(sa.Float, default=0.0)
@@ -28,7 +28,7 @@ class Candidate(db.Model, BaseUser):
     answers: orm.Mapped[list["CandidateAnswer"]] = orm.relationship()
 
     @classmethod
-    def authenticate(cls, db: orm.Session, git_hub_id: int) -> Self | None:
+    def authenticate(cls, db: orm.Session, git_hub_id: str) -> Self | None:
         user: Self | None = db.scalar(cls.select().where(cls.git_hub_id == git_hub_id))
         return user
 
